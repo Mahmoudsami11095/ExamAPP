e1a09bd65159d3259cb80b723c53a1534f7455b9
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2025-12-14 15:48:40 +0200

    Add Exam feature

---
+++ src/app/features/exams/models/exam.model.ts
@@ -0,0 +1,15 @@
+export interface Exam {
+    _id: string;
+    title: string;
+    duration: number;
+    subject: string;
+    numberOfQuestions: number;
+    active: boolean;
+    createdAt: string;
+}
+
+export interface ExamsResponse {
+    message: string;
+    metadata: any;
+    exams: Exam[];
+}
