c16dc99d2b2d863a22ceafa1c6f900e9239f16eb
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2025-12-21 02:00:47 +0200

    Use Zoneless to enhance Performance(Zone checks all components "Change Detection")

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -12,7 +12,7 @@ import { AUTH_BASE_URL } from 'auth';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([headerInterceptor, errorInterceptor])),
     provideAnimations(),
