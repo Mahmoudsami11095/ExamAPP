4f4dba71e24d0345b136956fd77102ea4280d30f
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2025-12-28 00:18:42 +0200

    group all adapters in one service

--- projects/auth/src/lib/auth.service.ts
+++ projects/auth/src/lib/auth.service.ts
@@ -3,10 +3,7 @@ import { catchError, map, Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { LoginResponse, StatusResponse, InfoResponse, MessageResponse } from './interfaces/auth-responses';
 import { LoginRequest, RegisterRequest, ForgotPasswordRequest, VerifyResetCodeRequest, ResetPasswordRequest } from './interfaces/auth-requests';
-import { AuthAPIAdaptorService } from './adaptor/auth-api.adaptor';
-import { AuthStatusAdaptorService } from './adaptor/auth-status.adaptor';
-import { AuthInfoAdaptorService } from './adaptor/auth-info.adaptor';
-import { AuthMessageAdaptorService } from './adaptor/auth-message.adaptor';
+import { AuthAdapterService } from './adaptor/auth.adapter';
 import { AuthEndPoint } from './enums/AuthEndPoint';
 import { AuthAPI } from './base/AuthAPI';
 import { AUTH_BASE_URL } from './tokens/auth-base-url.token';
@@ -18,10 +15,7 @@ import { AUTH_BASE_URL } from './tokens/auth-base-url.token';
 export class AuthService implements AuthAPI {
 
   _httpClient = inject(HttpClient);
-  _authAPIAdaptorService = inject(AuthAPIAdaptorService);
-  _authStatusAdaptorService = inject(AuthStatusAdaptorService);
-  _authInfoAdaptorService = inject(AuthInfoAdaptorService);
-  _authMessageAdaptorService = inject(AuthMessageAdaptorService);
+  _authAdapterService = inject(AuthAdapterService);
   private readonly baseUrl = inject(AUTH_BASE_URL);
 
   private getUrl(endpoint: string): string {
@@ -31,35 +25,35 @@ export class AuthService implements AuthAPI {
   register(data: RegisterRequest): Observable<LoginResponse> {
     return this._httpClient.post<LoginResponse>(this.getUrl(AuthEndPoint.REGISTER), data)
       .pipe(
-        map((response) => this._authAPIAdaptorService.adapt(response))
+        map((response) => this._authAdapterService.adaptLogin(response))
       );
   }
 
   login(data: LoginRequest): Observable<LoginResponse> {
     return this._httpClient.post<LoginResponse>(this.getUrl(AuthEndPoint.LOGIN), data)
       .pipe(
-        map((response) => this._authAPIAdaptorService.adapt(response))
+        map((response) => this._authAdapterService.adaptLogin(response))
       );
   }
 
   forgotPassword(data: ForgotPasswordRequest): Observable<InfoResponse> {
     return this._httpClient.post<InfoResponse>(this.getUrl(AuthEndPoint.FORGOTPASSWORD), data)
       .pipe(
-        map((response) => this._authInfoAdaptorService.adapt(response))
+        map((response) => this._authAdapterService.adaptInfo(response))
       );
   }
 
   verifyResetCode(data: VerifyResetCodeRequest): Observable<StatusResponse> {
     return this._httpClient.post<StatusResponse>(this.getUrl(AuthEndPoint.VERIFYRESETCODE), data)
       .pipe(
-        map((response) => this._authStatusAdaptorService.adapt(response))
+        map((response) => this._authAdapterService.adaptStatus(response))
       );
   }
 
   resetPassword(data: ResetPasswordRequest): Observable<MessageResponse> {
     return this._httpClient.put<MessageResponse>(this.getUrl(AuthEndPoint.RESETPASSWORD), data)
       .pipe(
-        map((response) => this._authMessageAdaptorService.adapt(response))
+        map((response) => this._authAdapterService.adaptMessage(response))
       );
   }
 
